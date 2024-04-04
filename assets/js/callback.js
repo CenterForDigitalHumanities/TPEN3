d4ee363cbada897cce944fd8743fee7c98bc140b
Onoja Victor <onoja.jsdev@gmail.com>
2024-04-03 19:06:34 -0500

    add callback stub

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -1,1 +1,37 @@
-alert("authentication callback complete.")
+// alert("authentication callback complete.")
+
+function redirectUser() {
+
+  let redirectUrl
+
+  const params = new URLSearchParams(window.location.search)
+  const referringUrl = decodeURIComponent(params.get("ref"))
+  // const hasProject = decodeURIComponent(params.get("projects"))
+  const userToken = decodeURIComponent(params.get("access-token"))
+
+  if (referringUrl) {
+    redirectUrl = referringUrl
+  } else if ( userHasProject(userToken)) {
+    redirectUrl = "xyz/dashboard/projects"
+  } else {
+    redirectUrl = "/"
+    // redirect to homepage with welcome texts and tutorials
+  }
+}
+
+const userHasProject = async (token) => {
+  try {
+    return await fetch("dev.api.tpen-services.org/project", {
+      method: post,
+      headers: {
+        Authorization: `Bearer ${window.TPEN_USER?.authorization}` || token,
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(userId)
+    })
+  } catch (error) {
+
+  }
+}
+
+window.onload = redirectUser()
