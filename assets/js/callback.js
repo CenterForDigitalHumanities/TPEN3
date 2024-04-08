e4a90b426e65fc9dfd87014ff3526d6e38420fff
Onoja Victor <111019083+jsvoo@users.noreply.github.com>
2024-04-08 13:06:39 -0500

    add callback stub (#42)
    * add callback stub
    
    * update referringPage, change userToken source
    
    * modify request method and access token, check response.ok
    
    * move hasProject checks to interfaces

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -1,1 +1,31 @@
-alert("authentication callback complete.")
+function redirectUser() {
+  let redirectUrl
+  const referringPage = getReferringPage()
+
+  if (referringPage && referringPage !== location.href) {
+    redirectUrl = referringPage
+    //We can change the current location t oprevent login loop. it'd take out all refs or params if we just location.href = referringPage for instance
+  } else{ 
+    redirectUrl = "https://www.tpen.org/interfaces"
+  }  
+}
+
+function getReferringPage() {
+  try {
+    const base64Hash = location.hash.split("state=")[1].split("&")[0]
+    const decodedUrl = b64toUrl(base64Hash)
+    return decodedUrl
+  } catch (err) {
+    return false
+  }
+}
+
+function urlToBase64(url) {
+  return window
+    .btoa(url)
+    .replace(/\//g, "_")
+    .replace(/\+/g, "-")
+    .replace(/=+$/, "")
+}
+
+window.onload = redirectUser()
