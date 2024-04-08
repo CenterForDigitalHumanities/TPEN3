326ae75d2609ed5dbe1370addaae25422e967eb6
Onoja Victor <onoja.jsdev@gmail.com>
2024-04-08 11:07:33 -0500

    move hasProject checks to interfaces

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -5,31 +5,9 @@ function redirectUser() {
   if (referringPage && referringPage !== location.href) {
     redirectUrl = referringPage
     //We can change the current location t oprevent login loop. it'd take out all refs or params if we just location.href = referringPage for instance
-  } else if (userHasProject()) {
-    redirectUrl = "xyz/dashboard/projects"
-  } else {
+  } else{ 
     redirectUrl = "https://www.tpen.org/interfaces"
-    // redirect to homepage with welcome texts and tutorials
-  }
-}
-
-async function userHasProject() {
-  try {
-    const response = await fetch(
-      "https://dev.api.tpen-services.org/project/myproject",
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${window.TPEN_USER?.authorization}`,
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      }
-    )
-
-    return response.ok ? response : false
-  } catch (error) {
-    return false
-  }
+  }  
 }
 
 function getReferringPage() {
