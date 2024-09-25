ecde1f0f490f0044e84242d7e792d7c7d85e669c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-09-25 15:43:12 -0500

    clean up and be ready for morning demo

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,3 @@
-/**
- * @module AuthButton Adds custom element for login/logout of Auth0, based on configuration below.
- * @author cubap
- * 
- * @description This module includes a custom `<button is="auth-button">` element for authentication within 
- * the Gallery of Glosses Project, specifically the data entry interfaces.
- * Notes: 
- * - Include this module and a button[is='auth-button'] element to use. 
- * - Add the `disabled` property on any page that should be available to the public, but knowing the user may be helpful.
- * - This can be made more generic by passing in the constants and parameterizing {app:'glossing'}.
- */
-
 // Import Auth0 library
 import 'https://cdn.auth0.com/js/auth0/9.19.0/auth0.min.js'
 
@@ -31,9 +19,9 @@ const webAuth = new auth0.WebAuth({
   state: urlToBase64(location.href),
 })
 
-const logout = () => {
-  webAuth.logout({ origin }) 
-}
+// const logout = () => {
+//   webAuth.logout({ origin }) 
+// }
 
 const login = (custom) =>
   webAuth.authorize(Object.assign({ authParamsMap: { app: "tpen" } }, custom))
@@ -48,67 +36,50 @@ const getReferringPage = () => {
 }
 
 /**
- * Follows the 'base64url' rules to decode a string.
- * @param {String} base64str from `state` parameter in the hash from Auth0
- * @returns referring URL
- */
-function b64toUrl(base64str) {
-    return window.atob(base64str.replace(/-/g, "+").replace(/_/g, "/"))
-}
-/**
- * Follows the 'base64url' rules to encode a string.
- * @param {String} url from `window.location.href`
- * @returns encoded string to pass as `state` to Auth0
- */
-function urlToBase64(url) {
-    return window.btoa(url).replace(/\//g, "_").replace(/\+/g, "-").replace(/=+$/, "")
-}
-
-/**
   * A user from a TPEN Interface at a third party source is trying to login.
   * They have initiated a https://three.t-pen.org/login from their source.
   * https://three.t-pen.org/ needs to perform a checkSession() for the user and follow the flow.
   * Once a token is known, three.t-pen.org will recieve that token and needs to pass it back into the interface it came from.
   * /login?redirectTo= gives the user the option to state where they would like to return.
   * Note this expects URL formatted like...
-  * 
-  /login/?redirectTo=https://transcribonanza.com/interface.html?customParam=hello#ididid
-  ^supported at this time
-  /login/#ididid?redirectTo=https://transcribonanza.com/interface.html?customParam=hello
-  ^unsupported at this time
+  
+    /login/?redirectTo=https%3A%2F%2Ftranscribonanza.com%2Finterface.html%3FcustomParam%3Dhello%23hashValue
+
+  *
 */
 function performLogin(){
+  // Know the value for ?redirectTo.  You have this whether the login returned here or is initiated here
   let redir = processRedirect()
+  // Know the value from state= after the universal login completes and redirects.  It will contain ?redirectTo which is where you need to go.
   let refer = getReferringPage()
+  // Know the ID token returned by a successful login in the universal login widget.  It is in the address bar as ?id_token=
   let idTok = location.hash.includes("id_token=") ? location.hash.split("id_token=")[1].split("&")[0] : ""
 
   // A login occurred and we came back to this page with the idToken and state
   if(idTok){
-    console.log("We logged in")
-    console.log(`redir: ${redir}`)
-    console.log(`refer: ${refer}`)
-
-    // We will have a refer to use to redirect.
+    // We will have refer, which is the decoded ?state=.  
     let referLink = new URL(refer)
     const referQueryString = referLink.search
     const referURLParams = new URLSearchParams(referQueryString)
 
-    // hmm the URL() does not consider the # to be a part of referLink.search but rather referLink.hash, which is wrong.
-    // We can split around the '#', or just accept referLink.hash just happens to do what we want.
-    const hackHash = referLink.hash
-
-    // The refer link has a redirect link as a URL parameter itself
+    // The decoded ?state= contains the ?redirectTo= that we needm the value of which which may also have URL parameters and/or a hash itself
     let redirect = referURLParams.get('redirectTo') ?? origin
+    redirect = decodeURI(redirect)
     let redirectLink = new URL(redirect)
     let redirectQueryString = redirectLink.search
 
+    // add idToken= into the redirect link next to any URL parameters it may have already contained
     if(redirectQueryString) redirectQueryString+=`&idToken=${idTok}`
     else redirectQueryString=`?idToken=${idTok}`
-    if(hackHash) redirectQueryString+=hackHash
+
+    // If the redirect link contains a hash, we would like that hash to appear at the end of the link after the query string(s)
+    if(redirectLink.hash) redirectQueryString+=redirectLink.hash
 
     location.href = redirectLink.origin + redirectLink.pathname + redirectQueryString
     return
   }
+
+  //Determine whether or not we need to login
   webAuth.checkSession({}, (err, result) => {
       if (err) {
           login() // Perform login if not authenticated.
@@ -117,12 +88,14 @@ function performLogin(){
       idTok = result.idToken ?? ""
       if (!idTok){
           console.error("There was missing token information from the login. Reset the cached User")
-          // TODO redirect still with some kind of error?
-          // No redirect but let them know an error happened.
+          // TODO don't need to redirect, but we should let the user know an error occurred.
           return
       }
-      // Redirect to the referring page, or the default page.
+
+      // We have an active session and were given an ID token.  We do not need to log in.
+      // Redirect to the referring page.  If there is no referring page b/c we didn't need to log in, use the redirect from processRedirect()
       if(!refer) refer = redir
+      refer = decodeURI(refer)
       let redirectLink = new URL(refer)
       let redirectQueryString = redirectLink.search
 
@@ -143,6 +116,7 @@ function processRedirect(url){
   const queryString = link.search
   const urlParams = new URLSearchParams(queryString)
   let redirect = urlParams.get('redirectTo') ?? origin
+  redirect = decodeURI(redirect)
   const hash = link.hash
   if(hash) redirect+=hash
   return redirect
