2f12cb10f367db34647b1afdb3bfdbc795aa6b0a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-10-17 14:57:23 -0500

    back to original function name

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -1,11 +1,11 @@
-window.onload = processRedirect()
+window.onload = redirectUser()
 
 /**
  *  Detect and get the value of returnTo from the origin address /callback/?returnTo=
  *  If there is no returnTo, default to the origin homepage.
  *  Note that performLogout() in auth.js will always add a returnTo, and it may just be the origin homepage.
  */
-function processRedirect() {
+function redirectUser() {
   let link = new URL(window.location.href)
   const queryString = link.search
   const urlParams = new URLSearchParams(queryString)
