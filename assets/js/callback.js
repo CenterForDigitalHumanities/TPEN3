0c0ba00eafaddc4ea0a36e3fda4cdad20ab2bfc0
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-10-24 09:19:33 -0500

    hotfix this loop

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -6,10 +6,18 @@ window.onload = redirectUser()
  *  Note that performLogout() in auth.js will always add a returnTo, and it may just be the origin homepage.
  */
 function redirectUser() {
-  let link = new URL(window.location.href)
+  let link = new URL(location.href)
   const queryString = link.search
   const urlParams = new URLSearchParams(queryString)
   let redirect = urlParams.get('returnTo') ?? location.origin
   redirect = decodeURI(redirect)
+  if(redirect === location.origin+"/logout/"){
+    alert("Please provide a ?returnTo= parameter when logging out.")
+    setTimeout(() => {
+      location.href = location.origin
+    }, "5000")
+    return
+  }
   location.href = redirect
+  return
 }
