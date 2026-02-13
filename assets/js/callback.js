02589e26e781f128e8971026619e7192eaf82ea3
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2026-02-13 14:08:52 -0600

    changes while reviewing

--- assets/js/callback.js
+++ assets/js/callback.js
@@ -7,16 +7,17 @@ function showReturnToOption() {
   const urlParams = new URLSearchParams(location.search)
   let redirect = urlParams.get('returnTo')
   if (!redirect) return
-  redirect = decodeURIComponent(redirect)
   try {
     redirect = new URL(redirect).toString()
-    const heading = document.createElement("h3")
+    const heading = document.createElement("h2")
     heading.innerText = "Back To Where I Came From"
     const link = document.createElement("a")
     link.href = redirect
     link.innerText = redirect
     const note = document.createElement("p")
-    note.innerText = "*Never click links you do not recognize."
+    const emphasis = document.createElement("em")
+    emphasis.innerText = "*Never click links you do not recognize."
+    note.appendChild(emphasis)
     const container = document.getElementById("returnToOption")
     if (!container) return
     container.appendChild(heading)
--- pages/callback.md
+++ pages/callback.md
@@ -9,7 +9,7 @@ You have been logged out of TPEN.  You may leave this page or close your browser
 
 Looking for somewhere to go next?  Check the links around the page or check out one of the options below. <br>
 
-### TPEN3 Interfaces Dashboard
+## TPEN3 Interfaces Dashboard
 [https://app.t-pen.org](https://app.t-pen.org)
 
 <div id="returnToOption"></div>
